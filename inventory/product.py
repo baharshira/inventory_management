29a2ee5c33692434f5302f5dbb64aa1a423b8976
baharshira <shirabahar@post.runi.ac.il>
2025-03-13 11:34:49 +0200

    implemented __hash__ and __eq__ to make Product hashable (for dictionary usage)

--- inventory/product.py
+++ inventory/product.py
@@ -6,3 +6,12 @@ class Product(BaseModel):
     description: str
     id: int
 
+    # Implementing __hash__ and __eq__ to make Product hashable
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+    def __eq__(self, other: any) -> bool:
+        if not isinstance(other, Product):
+            return False
+        return self.id == other.id  # Compare products by their ID
+
